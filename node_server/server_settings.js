77f66307d7422f77174f4398438eaaa186a16502
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-05-17 14:19:29 -0700

    Update server_settings.js

--- node_server/server_settings.js
+++ node_server/server_settings.js
@@ -7,7 +7,7 @@ const settings = {
     hotreload: 5000, //hotreload websocket server port
     python: 7000,  //quart server port
     python_node:7001, //websocket relay port (relays messages to client from nodejs that were sent to it by python)
-    startpage: 'spectrometer.html',  //home page
+    startpage: 'index.html',  //home page
     errpage: 'src/other/404.html', //error page, etc.
     certpath:'node_server/ssl/cert.pem',//if using https, this is required. See cert.pfx.md for instructions
     keypath:'node_server/ssl/key.pem'//if using https, this is required. See cert.pfx.md for instructions
